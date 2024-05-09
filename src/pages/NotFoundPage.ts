44a05de1b8506af8df2acdb53f7870935e1b2c97
DevSoon <jssosweet@gmail.com>
2024-05-09 10:30:56 +0900

    feat: 동적 라우트 적용

---
+++ src/pages/NotFoundPage.ts
@@ -0,0 +1,12 @@
+const NotFoundPage = () => {
+    const element = document.createElement('div');
+    element.innerHTML = `
+        <header>
+            <h1>NotFoundPage</h1>
+        </header>
+    `;
+
+    return element;
+};
+
+export default NotFoundPage;
--- src/utils/Router.ts
+++ src/utils/Router.ts
@@ -1,15 +1,40 @@
 import Home from '@/pages/Home';
 import About from '@/pages/About';
+import NotFoundPage from '@/pages/NotFoundPage';
 
-export const routes: { [key: string]: () => HTMLElement } = {
+
+type RouteParams = {
+    [key: string]: string;
+}
+
+export const routes: { [key: string]: (params?: RouteParams) => HTMLElement } = {
     '/': Home,
     '/about': About,
-    // '/contact': Contact,
+    '/:userId': About,
 };
 
+
 export function renderRoute(path: string) {
-    const PageComponent = routes[path] || Home;
-    return PageComponent();
+    const pathSegments = path.split('/').filter(p => p);
+    const matchedEntry = Object.keys(routes).find(route => {
+        const routeSegments = route.split('/').filter(p => p);
+        if (routeSegments.length !== pathSegments.length) return false;
+        return routeSegments.every((segment, index) => segment.startsWith(':') || segment === pathSegments[index]);
+    });
+
+    if (matchedEntry) {
+        const routeParams: RouteParams = {};
+        const routeSegments = matchedEntry.split('/').filter(p => p);
+        routeSegments.forEach((segment, index) => {
+            if (segment.startsWith(':')) {
+                const paramName = segment.slice(1);
+                routeParams[paramName] = pathSegments[index];
+            }
+        });
+
+        return routes[matchedEntry](routeParams);
+    }
+    return NotFoundPage();
 }
 
 export function navigateTo(path: string) {
